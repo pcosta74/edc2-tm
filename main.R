44fe24d28ba08f1f7b003dc0ed367e3577e42f78
Pedro Costa <pedro.mpm.costa@gmail.com>
2016-03-31 01:05:04 +0100

    Fix in pre-processing dtm.mx remove sparse terms

--- main.R
+++ main.R
@@ -26,12 +26,19 @@ readTab <- readTabular(mapping)
 corpus  <- Corpus(DataframeSource(FernandoPessoa), 
                   readerControl = list(reader = readTab, language = "pt"))
 
-# Pre-process
-skipWords <- function(x) removeWords(x, stopwords("portuguese"))
-functions <- list(tolower, removePunctuation, removeNumbers, stripWhitespace, skipWords)
+# Pre-processing
+skipWords <- function(x) removeWords(x, stopwords('portuguese'))
+rmvAccent <- function(x) chartr('àáâãéêíóõôúç','aaaaeeiooouc',x)
+functions <- list(content_transformer(tolower), #content_transformer(rmvAccent),
+                  removePunctuation, removeNumbers, stripWhitespace, skipWords)
 corpus.p  <- tm_map(corpus, FUN = tm_reduce, tmFuns = functions)
 
 
 # Create document term matrix
-mydtm <- DocumentTermMatrix(corpus.p, control = list(wordLengths = c(3,10)))
-inspect(mydtm)
+options <- list(minWordLength = 2,  minDocFreq=2, stemDocument=T, weighting=weightTfIdf)
+dtm.mx  <- DocumentTermMatrix(corpus.p, control = options)
+dtm.mx  <- removeSparseTerms(dtm.mx, 0.99)
+# inspect(dtm.mx)
+
+# Find top 10 frequent terms 
+freqterms10 <- findFreqTerms(dtm.mx, 10, 10)
